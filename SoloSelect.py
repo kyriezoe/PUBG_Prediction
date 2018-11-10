011b645eae831b2bf78bbd622dcbee56c4b01a11
kyrie <kyriezoe@outlook.com>
2018-11-10 15:56:07 -0600

    slight change

--- SoloSelect.py
+++ SoloSelect.py
@@ -1,4 +1,4 @@
 import pandas as pd
 df = pd.read_csv(u'test_V2.csv', encoding='gbk')
 outfile = df[(df[u'matchType']=='solo')]
-outfile.to_csv('test_solo.csv', index=False, encoding='gbk')
+outfile.to_csv('solo_test.csv', index=False, encoding='gbk')
